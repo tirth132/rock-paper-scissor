e09fb1edd120b1c7e0473b0c2ad2fd7779082e75
Tirth Patel <ptirth61@outlook.com>
2022-11-02 17:13:15 +0530

    Write function for playing a round of RPS

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function getComputerMove() {
   const number = Math.floor(Math.random() * 3);
   return MOVES[number];
 }
+
+function playGame(playerMove, computerMove) {
+  playerMove = playerMove.toLowerCase();
+
+  const move1 = MOVES.indexOf(playerMove);
+  const move2 = MOVES.indexOf(computerMove);
+
+  if (move2 == move1 + 1 || move2 == move1 - 2) {
+    return `You lost! ${computerMove} beats ${playerMove}`;
+  } else if (move1 == move2 + 1 || move1 == move2 - 2) {
+    return `You won! ${playerMove} beats ${computerMove}`;
+  } else {
+    return `It's a draw!`;
+  }
+}
